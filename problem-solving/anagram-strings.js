6969574b2a12e898f18db4be62f37796f0110b51
iqans <iqan.shaikh@gmail.com>
2021-02-19 23:06:02 +0000

    anagram solution added

---
+++ problem-solving/anagram-strings.js
@@ -0,0 +1,23 @@
+function validAnagram(str1, str2){
+  if(str1.length !== str2.length) {
+      return false;
+  }
+  let storage = {};
+  for(let c of str1) {
+      storage[c] = ++storage[c] || 1;
+  }
+  for(let c of str2) {
+      if(storage[c]) {
+          storage[c]--;
+          if(storage[c] < 0) {
+              return false;
+          }
+      } else {
+          return false;
+      }
+  }
+  return true;
+}
+
+console.log(validAnagram('hello', 'olehl'));
+console.log(validAnagram('', ''));
