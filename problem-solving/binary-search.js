89a3ecc0f7523658e0a1069542370b8551345afe
iqans <iqan.shaikh@gmail.com>
2021-02-20 18:10:59 +0000

    binary search added

---
+++ problem-solving/binary-search.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ * binary search in sorted array
+ * 
+ */
+
+function binarySearch(arr, item){
+  // add whatever parameters you deem necessary - good luck!
+  let left = 0;
+  let right = arr.length - 1;
+  while(left <= right) {
+      let medium = Math.floor((right + left) / 2);
+      if(item === arr[medium]) return medium;
+      if(item < arr[medium]) {
+          right = medium - 1;
+      } else {
+          left = medium + 1;
+      }
+  }
+  return -1;
+}
+
+console.log(binarySearch([1,2,3,4,5,6,7], 2));
+console.log(binarySearch([1,2,3,4,5,6,7], 1));
+console.log(binarySearch([1,2,3,4,5,6,7], 7));
